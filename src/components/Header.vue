1639ae25808bb964eb7e6f57dc95b4670cd30039
sunyu1 <sunyu1@green-valley.com>
2019-12-19 10:22:17 +0800

    add

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,31 +2,57 @@
   <div class="page-header">
     <div class="header-content">
       <div class="home-logo">
-        <img class="logo-img" src="../assets/logo@2x.png" alt="">
+        <img class="logo-img" src="../assets/logo@2x.png" alt="" />
         <span class="logo-text">燧人医疗</span>
       </div>
       <div class="home-search">
-        <div class="search-input"><input  type="text" placeholder="搜索药品，如「阿托伐他汀钙片」"></div>
-        <div class="search-btn"><img src="../assets/search.png" alt=""></div>
+        <div class="search-input">
+          <input type="text" placeholder="搜索药品，如「阿托伐他汀钙片」" />
+        </div>
+        <div class="search-btn"><img src="../assets/search.png" alt="" /></div>
       </div>
       <div class="hone-navbar">
-        <router-link to="/" tag="div" class="nav-item" :class="{'active': isActive=== 'home'}">首页</router-link>
-        <router-link to="/home" tag="div" class="nav-item" :class="{'active': isActive=== 'home'}">药品信息</router-link>
-        <router-link to="/informationReport" tag="div" class="nav-item" :class="{'active': isActive=== 'informationReport'}">信息报告</router-link>
-        <router-link to="/aboutus" tag="div" class="nav-item" :class="{'active': isActive=== 'aboutus'}">关于我们</router-link>
+        <router-link
+          to="/"
+          tag="div"
+          class="nav-item"
+          :class="{ active: isActive === 'homeInfo' }"
+          >首页</router-link
+        >
+        <router-link
+          to="/home/drugsDetails"
+          tag="div"
+          class="nav-item"
+          :class="{ active: isActive === 'drugsDetails' }"
+          >药品信息</router-link
+        >
+        <router-link
+          to="/informationReport"
+          tag="div"
+          class="nav-item"
+          :class="{ active: isActive === 'informationReport' }"
+          >信息报告</router-link
+        >
+        <router-link
+          to="/aboutus"
+          tag="div"
+          class="nav-item"
+          :class="{ active: isActive === 'aboutus' }"
+          >关于我们</router-link
+        >
       </div>
     </div>
   </div>
 </template>
 <script>
 export default {
-  data() {
+  data () {
     return {
       isActive: 'home'
     }
   },
-  watch:{
-    $route(to,from){
+  watch: {
+    $route (to, from) {
       this.isActive = to.name
     }
   },
@@ -34,103 +60,103 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-.page-header{
+.page-header {
   width: 100%;
   height: 100px;
   background: #ffffff;
   box-sizing: border-box;
-  .header-content{
+  .header-content {
     margin: 0 auto;
     width: 1200px;
     height: 100%;
     display: flex;
     flex-flow: row nowrap;
     align-items: center;
-    .home-logo{
+    .home-logo {
       display: flex;
       flex-flow: row nowrap;
       align-items: center;
       margin-left: 20px;
-      .logo-img{
+      .logo-img {
         width: 50px;
         height: 50px;
       }
-      .logo-text{
-        font-size:22px;
-        font-weight:bold;
-        color:rgba(86,133,245,1);
+      .logo-text {
+        font-size: 22px;
+        font-weight: bold;
+        color: rgba(86, 133, 245, 1);
         display: inline-block;
         margin-left: 10px;
       }
     }
-    .home-search{
+    .home-search {
       width: 452px;
       height: 42px;
-      background:rgba(255,255,255,1);
-      border-radius:24px;
-      border:2px solid rgba(75,143,248,1);
+      background: rgba(255, 255, 255, 1);
+      border-radius: 24px;
+      border: 2px solid rgba(75, 143, 248, 1);
       margin-left: 80px;
       box-sizing: border-box;
       display: flex;
       flex-flow: row nowrap;
       justify-content: center;
-      
-      .search-input{
+
+      .search-input {
         box-sizing: border-box;
         flex: 1;
-        height:38px;
+        height: 38px;
         /* width: 384px; */
         padding-left: 31px;
         padding-top: 7px;
         padding-bottom: 8px;
         padding-right: 10px;
-        input{
+        input {
           height: 100%;
           width: 100%;
-          outline-style: none ;
+          outline-style: none;
           border: 0px;
           /* width:280px; */
-          font-size:16px;
+          font-size: 16px;
         }
       }
-      .search-btn{
+      .search-btn {
         box-sizing: border-box;
-        width:64px;
+        width: 64px;
         height: 38px;
         text-align: center;
-        background:rgba(75,143,248,1);
-        border-radius:24px;
+        background: rgba(75, 143, 248, 1);
+        border-radius: 24px;
         cursor: pointer;
         margin-right: -2px;
-        img{
+        img {
           padding-top: 10px;
         }
       }
-      .search-btn:active{
+      .search-btn:active {
         padding-top: 2px;
         padding-left: 2px;
         background: #3a8ee6;
       }
     }
-    .hone-navbar{
+    .hone-navbar {
       margin-left: 64px;
       display: flex;
       flex-flow: row nowrap;
       align-items: center;
-      font-size:18px;
+      font-size: 18px;
       font-weight: 400;
-      color:rgba(40,40,40,1);
-      font-family:PingFang-SC-Medium,PingFang-SC;
-      
-      .nav-item{
+      color: rgba(40, 40, 40, 1);
+      font-family: PingFang-SC-Medium, PingFang-SC;
+
+      .nav-item {
         margin-right: 32px;
         cursor: pointer;
       }
-      .active{
-        font-size:18px;
-        font-family:PingFangSC-Semibold,PingFang SC;
-        font-weight:600;
-        color:rgba(75,143,248,1);
+      .active {
+        font-size: 18px;
+        font-family: PingFangSC-Semibold, PingFang SC;
+        font-weight: 600;
+        color: rgba(75, 143, 248, 1);
       }
     }
   }
